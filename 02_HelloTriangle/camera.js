219c930ab6d5d04cb6b5c83fd4a73837dd91a72b
Kostja <k.gontscharow@gmail.com>
2015-05-26 16:20:23 +0200

    camera now with projection matrix

--- 02_HelloTriangle/camera.js
+++ 02_HelloTriangle/camera.js
@@ -2,29 +2,27 @@
  * Created by kostja93 on 26.05.15.
  */
 var Camera = function (matrix) {
-    this.matrix = matrix;
     this.projection = matrix;
-    console.log(matrix);
 };
 
 Camera.prototype.rotateX = function (radians) {
-    mat4.rotateX(this.matrix, this.matrix, radians);
+    mat4.rotateX(this.projection, this.projection, radians);
 };
 
 Camera.prototype.rotateY = function (radians) {
-    mat4.rotateY(this.matrix, this.matrix, radians);
+    mat4.rotateY(this.projection, this.projection, radians);
 };
 
 Camera.prototype.rotateZ = function (radians) {
-    mat4.rotateZ(this.matrix, this.matrix, radians);
+    mat4.rotateZ(this.projection, this.projection, radians);
 };
 
 Camera.prototype.perspective = function (fudgeFactor) {
-    var perspactiveMatrix = [
+    var perspactiveProjection = [
         1, 0, 0, 0,
         0, 1, 0, 0,
         0, 0, 1, fudgeFactor,
         0, 0, 0, 1
     ];
-    mat4.multiply(this.projection, this.projection, perspactiveMatrix);
+    mat4.multiply(this.projection, this.projection, perspactiveProjection);
 };
--- 02_HelloTriangle/webglstart.js
+++ 02_HelloTriangle/webglstart.js
@@ -102,9 +102,6 @@ function drawScene() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    mat4.identity(projectionMatrix);
-    mat4.identity(modelViewMatrix);
-
     gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, projectionMatrix);
     gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, modelViewMatrix);
 
