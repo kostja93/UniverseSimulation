82161bdf3210468e6ff62d13c515e5010799eed2
Kostja <k.gontscharow@gmail.com>
2015-06-17 13:57:55 +0200

    added textures files

---
+++ js/gamecontroller.js
@@ -0,0 +1,80 @@
+// https://developer.mozilla.org/en-US/docs/Web/Guide/API/Gamepad
+
+var GameController = function(node){
+    // Knoten der durch den Controller gesteuert werden soll (z.B. die Kamera)
+    this.node = node;
+
+    this.registerEvents();
+}
+
+GameController.prototype.registerEvents = function(){
+    
+    window.addEventListener("gamepadconnected", function(e) { gp.connecthandler(e.gamepad);});
+    window.addEventListener("gamepaddisconnected", function(e) { gp.disconnecthandler(e.gamepad);});
+
+    var gp = new Object();
+    gp.connected = this.connected;
+    
+    gp.connecthandler = function(gamepad) {
+        console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+          gamepad.index, gamepad.id, gamepad.buttons.length, gamepad.axes.length);
+        gp.controller = gamepad;
+    }
+    gp.disconnecthandler = function(gamepad) {
+        if(gp.controller.index == gamepad.index){
+          console.log("Gamepad disconnected from index %d: %s", gamepad.index, gamepad.id);
+          delete gp.controller;
+        }
+    }
+
+    this.gp = gp;
+
+}
+
+GameController.prototype.isConnected = function() {
+    return typeof this.gp.controller != "undefined";
+}
+
+GameController.prototype.buttonAction = function(buttonidx){
+    // Beispielcode
+    console.log("Button pressed:", buttonidx);
+
+    /**
+     Hier Code einfügen
+    **/
+}
+
+GameController.prototype.axesAction = function(axes){
+    // Beispielcode zum auslesen der Werte von Stick 3
+    if(axes[3].toFixed(3)>0.1||axes[3].toFixed(3)<-0.1){
+      console.log("Axis 3:", axes[3].toFixed(3));
+    }
+
+    /**
+     Hier Code einfügen
+    **/
+}
+
+// updateStatus() wird vor jedem Zeichnen der Szene aufgerufen
+GameController.prototype.updateStatus = function() {
+    
+    if(!this.isConnected())
+        return;
+
+    var controller = this.gp.controller;
+
+    for (i = 0; i < controller.buttons.length; i++){
+        var val = controller.buttons[i];
+        var pressed = val == 1.0;
+        if (typeof(val) == "object"){
+            pressed = val.pressed;
+            val = val.value;
+        }
+
+        if (pressed){
+            this.buttonAction(i);
+        } 
+    }
+
+    this.axesAction(controller.axes);   
+}
---
+++ js/light/texture.js
@@ -0,0 +1,42 @@
+var Texture = function(src){
+    Node.call(this);
+
+    this.image = new Image();
+    this.image.texture = gl.createTexture();
+    this.image.onload = function() { 
+        Texture.loadTexture(this, this.texture);
+        console.log("Texture " + src + " loaded." );
+    };
+    this.image.onerror = function() { 
+        console.warn("Could not load texture: " + src);
+    };
+    this.image.src = src;
+    
+    this.texture = this.image.texture;
+}
+
+//Inherit from Object3d/SGNode
+Texture.prototype = Object.create(Node.prototype);
+Texture.prototype.constructor = Node;
+
+//Add Functions to the Node prototype
+
+Texture.loadTexture = function(image, texture){
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    
+    //Erlaube alle Größen von textures
+    //(idealerweise haben textures die Seitengröße x^2)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE); 
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE); 
+
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+Texture.prototype.draw = function(){
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    gl.uniform1i(gl.getUniformLocation(shaderProgram, "uSampler"), 0);
+}
